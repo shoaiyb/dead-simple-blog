9c1c8165cef683594dbba3cc455368cdf2d5dbc5
shoaiyb sysa <75518689+shoaiyb@users.noreply.github.com>
2021-06-06 21:02:24 +0100

    Update theme.php

--- themes/default/theme.php
+++ themes/default/theme.php
@@ -3,19 +3,19 @@
 <head>
 	<title><?php if ( !empty($_GET['post']) ) { echo $post_title.' - '; } ?><?php echo $blog_title; ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="style.css" />
+	<link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
 	<header>
-		<h1 class="blog-title"><a href="<?php echo $base_url; ?>"><?php echo $blog_title; ?></a></h1>
+		<h1 class="blog-title"><a href="<?= $base_url; ?>"><?= $blog_title; ?></a></h1>
 		<hr />
 	</header>
 	<article>
-		<?php echo $content; ?>
+		<?= $content; ?>
 	</article>
 	<footer>
 		This blog does not offer comment functionality. If you'd like to discuss any of the topics 
-		written about here, you can <a href="mailto:<?php echo $contact_email; ?>">send an email</a>.
+		written about here, you can <a href="mailto:<?= $contact_email; ?>">send an email</a>.
 	</footer>
 </body>
 </html>
