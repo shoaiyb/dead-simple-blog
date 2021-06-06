3e646c242711546e3cf1e282785abd52f3af4bde
shoaiyb sysa <75518689+shoaiyb@users.noreply.github.com>
2021-06-06 20:55:56 +0100

    Update config.php

--- config.php
+++ config.php
@@ -3,11 +3,11 @@
 // URL of the homepage. Can be absolute or relative.
 $base_url = '/blog/';
 
-// Blog title
+// Blog title.
 $blog_title = 'Painted Sky Blog';
 
-// Admin email
+// Admin email.
 $contact_email = 'blog@paintedsky.ca';
 
-// Use amp, true to enable / false or otherwise to disable.
-$use_amp = 'false';
+// Blog theme.
+$theme = 'default';
