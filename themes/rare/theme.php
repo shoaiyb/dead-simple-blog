33de704c578bdd37994bb1f85ef39d1caba6dca8
shoaiyb sysa <75518689+shoaiyb@users.noreply.github.com>
2021-07-18 12:43:04 +0100

    Create theme.php

---
+++ themes/rare/theme.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php if ( !empty($_GET['post']) ) { echo $post_title.' - '; } ?><?php echo $blog_title; ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+	<header>
+		<h1 class="title"><a href="<?= $base_url; ?>"><?= $blog_title; ?></a></h1>
+		<hr />
+	</header>
+	<article class="content">
+		<?= $content; ?>
+	</article>
+	<footer class="footer">
+		This blog does not offer comment functionality. If you'd like to discuss any of the topics 
+		written about here, you can <a href="mailto:<?= $contact_email; ?>">send an email</a>.
+	</footer>
+</body>
+</html>
